331341a04026f50e2580c65066c66419fe97c079
sjohnp2112 <99998544+sjohnp2112@users.noreply.github.com>
2022-09-09 12:54:58 -0400

    Add files via upload
    start of project, Question 1 in notebook: Do males have longer trip durations on average than females?

---
+++ Explore_bikeshare_data.r
@@ -0,0 +1,29 @@
+
+ny = read.csv('new_york_city.csv')
+wash = read.csv('washington.csv')
+chi = read.csv('chicago.csv')
+
+head(ny)
+
+summary(ny)
+
+
+head(wash)
+
+head(chi)
+
+# Your solution code goes here
+vector1 <- c(ny$trip.duration)
+
+vector1
+
+
+
+
+
+# Your solution code goes here
+
+
+# Your solution code goes here
+
+system('python -m nbconvert Explore_bikeshare_data.ipynb')
