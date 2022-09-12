9b03eaf0963f60ec8919e094201bcd97d84e6e58
sjohnp2112 <99998544+sjohnp2112@users.noreply.github.com>
2022-09-12 12:22:27 -0400

    Add files via upload
    can't find 'gender' column..

--- Explore_bikeshare_data.r
+++ Explore_bikeshare_data.r
@@ -5,19 +5,27 @@ chi = read.csv('chicago.csv')
 
 head(ny)
 
-summary(ny)
+# summary(ny)
 
 
-head(wash)
+# head(wash)
 
-head(chi)
+# head(chi)
+
+dim(ny)
+
+library('ggplot2')
+names(ny)
 
 # Your solution code goes here
-vector1 <- c(ny$trip.duration)
 
-vector1
+# qplot(x = Trip.Duration, data = ny)
 
 
+qplot(x = duration, data = subset(ny, !is.na(gender)),
+      binwidth = 10) +
+    scale_x_continuous(lim = c(0, 1000), breaks = seq(0, 100, 10))+
+    facet_wrap(~gender)
 
 
 
