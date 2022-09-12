df8017455c844fe432e1a1ce5439db6a9694adee
sjohnp2112 <99998544+sjohnp2112@users.noreply.github.com>
2022-09-12 13:28:55 -0400

    Add files via upload

--- Explore_bikeshare_data.r
+++ Explore_bikeshare_data.r
@@ -19,20 +19,14 @@ names(ny)
 
 # Your solution code goes here
 
-# qplot(x = Trip.Duration, data = ny)
-
-
 qplot(x = Trip.Duration, data = subset(ny, !is.na(Gender)),
       binwidth = 20) +
-    scale_x_continuous(lim = c(0, 500), breaks = seq(0, 500, 100))+
+    scale_x_continuous(lim = c(0, 1000), breaks = seq(0, 1000, 250))+
     facet_wrap(~Gender)
 
-# ggplot(aes(x = dob_day), data = fb) +
-#   geom_histogram(binwidth = 1) +
-#   scale_x_continuous(breaks = 1:31) +
-#   facet_wrap(~gender) 
 
-
+table(ny$Gender)
+by(ny$Trip.Duration, ny$Gender, summary)
 
 # Your solution code goes here
 
