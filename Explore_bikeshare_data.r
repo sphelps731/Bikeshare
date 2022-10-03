3b62865d5fe597956b4067e5d1aabfe668a24aea
sjohnp2112 <99998544+sjohnp2112@users.noreply.github.com>
2022-10-03 13:35:35 -0400

    Add files via upload
    latest file

--- Explore_bikeshare_data.r
+++ Explore_bikeshare_data.r
@@ -154,15 +154,22 @@ head(end_stat_sum)
 # head(end_stat_sum)
 
 
-#function that return
+#function that returns most popular end stations
 popular_stations <- function(df) { 
     
     df <- transform(df, freq= ave(seq(nrow(df)), End.Station, FUN=length))      
 
-#     df[order(-df$freq),]
+    df[order(-df$freq),]
     
-    sort(unique(df$freq), decreasing = TRUE)
-
+    Result <- sort(unique(df$freq), decreasing = TRUE)
+    EndStation <- head(df$End.Station, 10)  #c(df$End.Station)
+    
+#     new_df <- data.frame(EndStation, Result)
+#     
+    
+       
+#     return(df_new)
+    print(EndStation)
 }
 
 
